d52b41f19fbf595aea39e547bd775a90c46515e4
LeaHaelin <lea8haelin@gmail.com>
2023-08-17 18:54:35 +0200

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const loginForm = document.querySelector(".login");
 const loginInput = document.querySelector(".login__input");
+const greeting = document.querySelector("#greeting");
 
-console.log(loginInput)
+const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "userName"
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASS);
+    loginForm.addEventListener("submit", loginSubmitHandler)
+} else {
+    //show the greetings
+    paintGreeting(savedUserName);
+}
 
 function loginSubmitHandler(event) {
     event.preventDefault();
     const userName = loginInput.value;
-    console.log(event)
+    loginForm.classList.add(HIDDEN_CLASS);
+    localStorage.setItem(USERNAME_KEY, userName);
+    paintGreeting(userName)
 }
 
 loginForm.addEventListener("submit", loginSubmitHandler);
+
+function paintGreeting(userName) {
+    greeting.classList.remove(HIDDEN_CLASS);
+    greeting.innerHTML = `Hello ${userName}!`
+}
+
+
+
