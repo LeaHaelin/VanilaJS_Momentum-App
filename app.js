cc0f26095333b335eebfba0564f0cb99a63eae44
LeaHaelin <lea8haelin@gmail.com>
2023-08-22 19:20:28 +0200

    part_login

--- app.js
+++ app.js
@@ -2,33 +2,27 @@ const loginForm = document.querySelector(".login");
 const loginInput = document.querySelector(".login__input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASS = "hidden";
-const USERNAME_KEY = "userName"
-const savedUserName = localStorage.getItem(USERNAME_KEY);
-
-if (savedUserName === null) {
-    //show the form
-    loginForm.classList.remove(HIDDEN_CLASS);
-    loginForm.addEventListener("submit", loginSubmitHandler)
-} else {
-    //show the greetings
-    paintGreeting(savedUserName);
-}
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function loginSubmitHandler(event) {
-    event.preventDefault();
-    const userName = loginInput.value;
-    loginForm.classList.add(HIDDEN_CLASS);
-    localStorage.setItem(USERNAME_KEY, userName);
-    paintGreeting(userName)
+    event.preventDefault(); //prevent submit automatically
+    loginForm.classList.add(HIDDEN_CLASSNAME); //to add class hidden to make form disappear 
+    const user = loginInput.value;//to save the input value
+    localStorage.setItem(USERNAME_KEY, user)//save username in localStorage
+    paintGreeting(user);
 }
 
-loginForm.addEventListener("submit", loginSubmitHandler);
+function paintGreeting(user) {
+    greeting.innerHTML = `Hi ${user}`;//it will show hi with the user's name
+    greeting.classList.remove(HIDDEN_CLASSNAME); //to show greeting item after the form is gone
+}  //those two lines were repeated. so make it a function and replace repeating in loginSubmit to this function
 
-function paintGreeting(userName) {
-    greeting.classList.remove(HIDDEN_CLASS);
-    greeting.innerHTML = `Hello ${userName}!`
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+//to get the localStorage item
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", loginSubmitHandler);
+} else {
+    paintGreeting(savedUsername);
 }
-
-
-
